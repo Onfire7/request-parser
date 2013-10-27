c43a31a8a96317612fc4359f51568e12b441e206
Sean Goheen <github@onfire.us>
2013-10-27 03:21:21 +0000

    Added POST method for non binary uploads.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var url = require('url');
+var qs = require('querystring');
 
 function Parse (func) {
 	return function(req, res){
@@ -8,7 +9,30 @@ function Parse (func) {
 		if (parts.query) req.get = parts.query;
 		
 		// Handle POST variables
-		
+		if (req.method.toUpperCase() === 'POST') {
+			
+			req.postData = "";
+			
+			req.on('data', function(data){
+				req.postData += data;
+				
+				// Check data size
+				if (req.postData.length > 1e7) { // 10MB limit
+					req.postData = '';
+					response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+					request.connection.destroy();
+				}
+			});
+			
+			req.on('end', function(){
+				// If not posted as binary data
+				if (req.headers['Content-Type'].toLowerCase()  !== 'multipart/form-data' ) {
+					req.post = querystring.parse(postdata);
+				}else{
+					
+				}
+			});
+		}
 		
 		func(req, res);
 	};
